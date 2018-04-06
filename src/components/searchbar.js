6d610bb1de00979565192183fa29b6b042e8347b
Dylan Ellison <dylancellison@gmail.com>
2018-04-06 00:20:28 -0700

    switched function to arrow function

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input onChange={this.onInputChange} />;
+        return <input onChange={ event => console.log(event.target.value)} />;
     }
-
-    onInputChange(event) {
-        console.log(event.target.value)
-
-    }
+    // old code passed into onChange originally
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    //     console.log(event)
+    //
+    // }
 
 }
 
