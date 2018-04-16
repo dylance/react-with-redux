5b93b9e15e07f26aa70db17e1bda885da7dffdea
Dylan Ellison <dylancellison@gmail.com>
2018-04-15 22:30:46 -0700

    reformatted code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/searchbar'
 
 const API_KEY = 'AIzaSyAu4rwuUSWunPW-zoghnFa1Hn9iOErA_hU'
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
     console.log(data);
-})
+});
+
 // Create a new component
 // This component will produce some HTML
 // function() == () =>
