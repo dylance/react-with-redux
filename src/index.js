5dad94ef926a5e71db9da37f7cccbceeb40081c5
Dylan Ellison <dylancellison@gmail.com>
2018-04-15 22:47:10 -0700

    added AJAX response to state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar'
 
 const API_KEY = 'AIzaSyAu4rwuUSWunPW-zoghnFa1Hn9iOErA_hU'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-});
+
 
 // Create a new component
 // This component will produce some HTML
 // function() == () =>
-const App = () => {
-    return (
-        <div>
-          <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {videos: []};
+
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+          this.setState({videos: videos});
+      });
+    }
+
+    render() {
+      return (
+          <div>
+            <SearchBar />
+          </div>
+      );
+  }
 }
 
 // Take this component's generated HTML and
