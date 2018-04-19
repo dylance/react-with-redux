61942185f627c6673c2ebb00c4f7e69b1b890a65
Dylan Ellison <dylancellison@gmail.com>
2018-04-18 22:10:56 -0600

    added thumbnail to video list

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,18 +4,19 @@ const VideoListItem = (props) => {
   const video = props.video;
   // ES6 code. line below  allows you to remove above line of acode
 //  const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imageUrl}/>
         </div>
         <div className="media-body">
-          <div className="media-heading"></div>  
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
-    Video
+
     </li>
   );
 
