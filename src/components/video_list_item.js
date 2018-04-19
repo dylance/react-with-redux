44db439e92a0f17c91c3fee2b502562559d39f4c
Dylan Ellison <dylancellison@gmail.com>
2018-04-18 22:05:04 -0600

    added markup

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -6,7 +6,17 @@ const VideoListItem = (props) => {
 //  const VideoListItem = ({video}) => {
 
   return (
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading"></div>  
+        </div>
+      </div>
+    Video
+    </li>
   );
 
 };
