9574af5b4d71fd2e5a0b25a98300501b70380e35
Dylan Ellison <dylancellison@gmail.com>
2018-04-18 21:57:34 -0600

    added ES6 syntax with comment

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
+  const video = props.video;
+  // ES6 code. line below  allows you to remove above line of acode
+//  const VideoListItem = ({video}) => {
+
   return (
     <li>Video</li>
   );
